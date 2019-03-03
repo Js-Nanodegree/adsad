4766eada9720236a372c0977b27239158b23b2ad
Slava Yakimov <js-nanodegree@outlook.com>
2019-03-03 18:42:22 +0500

    webpack clean point

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-   entry: {
-     app: './src/index.js',
-     print: './src/print.js'
-   },
-  output: {
-    filename: '[name].bundle.js',
-   path: path.resolve(__dirname, 'dist')
- }
+    mode: 'development',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+             contentBase: './dist'
+           },
+    plugins: [
+        new CleanWebpackPlugin(['dist/*']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
 };
