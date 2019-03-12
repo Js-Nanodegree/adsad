d23b83e7a58dcd43837c1a34e038eb9b26845c4d
VirTEK11 <jsnanodegree@gmail.com>
2019-03-12 15:43:34 +0500

    WebSocket Init

--- ws.js
+++ ws.js
@@ -1,65 +1,43 @@
-// /*****  Setup a GraphQL subscription observable  ******************************/
+const { PubSub } =require ('apollo-server')
 
-const {execute} = require('apollo-link');
-const {WebSocketLink} = require('apollo-link-ws');
-const {SubscriptionClient} = require('subscriptions-transport-ws');
-const ws = require('ws');
+const WebSocket = require('ws');
+const ws = new WebSocket('ws://192.168.0.20:8090');
 
-const getWsClient = function(wsurl) {
-  const client = new SubscriptionClient(
-    wsurl, {
-      reconnect: true
-    }, ws
-  );
-  return client;
-};
-const createSubscriptionObservable = (wsurl, query, variables) => {
-  const link = new WebSocketLink(getWsClient(wsurl));
-  return execute(link, {
-    query: query,
-    variables: variables
-  });
-};
+const pubsub = new PubSub()
+const MESSAGE_CREATED = 'MESSAGE_CREATED'
 
-const gql = require('graphql-tag');
-// A subscription query to get changes for author with parametrised id 
-// using $id as a query variable
-const SUBSCRIBE_QUERY = gql`
-subscription{messageCreated{message{result{status} error id}}}
-`;
+const resolvers = {
+	Query: {
+		messages: () => [{ id: 0, content: 'Hello!' }, { id: 1, content: 'Bye!' }],
+	},
+	Subscription: {
+		messageCreated: {
+			subscribe: () => pubsub.asyncIterator(MESSAGE_CREATED),
+		},
+	},
+}
 
-const main = () => {
-  const subscriptionClient = createSubscriptionObservable(
-    'ws://localhost:4000/graphql', // GraphQL endpoint
-    SUBSCRIBE_QUERY, // Subscription query
-  // Query variables
-  );
-  subscriptionClient.subscribe(data => {
+const msg = JSON.stringify({
+	id:200,
+  method: 'order.subscribe',
+  params: [84,"BTCUSD"],
+})
+
+ 
+ws.on('open', function open() {
+  ws.send(msg);
+});
+
+ 
+ws.on('message', function incoming(data) {
+	const dat = JSON.parse(data)
+	pubsub.publish(MESSAGE_CREATED, {
+		messageCreated:{message:dat },
+	})
 	console.log(data)
-})}
 
-main()
+});
 
- 
- 
-// wss.on('open', function open() {
-// 	wss.send('something');
-// });
- 
-// wss.on('message', function incoming(data) {
-// 	const main = (data) => {
-// 		const subscriptionClient = createSubscriptionObservable(
-// 		  'ws://localhost:4000/graphql', // GraphQL endpoint
-// 		  SUBSCRIBE_QUERY, // Subscription query
-// 		// Query variables
-// 		);
-// 		subscriptionClient.subscribe(data => {
-// 			console.log(data)
-// 		  // Do something on receipt of the event
-// 		  // console.log("Received event: ");
-// 		  console.log({a:data})
-		
-// 	  })}
-	  
-// 	  main()
-// });
+
+
+module.exports = resolvers
